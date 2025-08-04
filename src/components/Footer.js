3402678cb19d1bfe74ed8ac56524c8fb851c69f6
henry-leavey <henry_leavey@outlook.com>
2025-08-04 13:05:02 +0100

    navbar fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,14 +9,9 @@ export const Footer = () => {
     <footer className="footer">
       <Container>
         <Row className="align-items-center">
-          <Col size={12} sm={6} className="text-center text-sm-end">
-            <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
-            </div>
+          <span className="navbar-text">
             <p>Copyright 2025. All Rights Reserved</p>
-          </Col>
+          </span>
         </Row>
       </Container>
     </footer>
